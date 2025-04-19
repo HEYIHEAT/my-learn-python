bba950e68e41e28346164366daa0469a4228841e
HEYIHEAT <heyiheat@gmail.com>
2025-04-20 00:08:11 +0800

    Add files via upload

---
+++ 变量.py
@@ -0,0 +1,28 @@
+"""
+多行注释,
+对python文件,类,方法进行解释(所以一般三个引号都是在开头)
+
+"""
+
+"""
+帅哥
+"""
+"""
+print("helloworld")
+print("helloworld")
+
+
+
+print("helloworld")
+"""
+
+
+# print(666)
+money = 50
+print("钱包余额为:", money,"多说一句,你真是个穷光蛋,加油吧孩子!以后争取一分钟就赚",money,"块")
+
+money = money - 10
+print("你花了十元买了一个冰淇凌,余额还有",money,"元")
+money = money - 20
+print("花费20元买了个玩具","余额还有",money,"元")
+print("你的余额为",money)
